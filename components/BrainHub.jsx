69c84b5d125402d570bde1616bc4d12e497a2cbe
hiiD4vd <mahezadaudabubakar@gmail.com>
2026-07-02 11:30:00 +0700

    feat: add BrainHub central SVG component

---
+++ components/BrainHub.jsx
@@ -0,0 +1,21 @@
+"use client";
+export default function BrainHub() {
+  return (
+    <div className="brain-hub">
+      <div id="brain-anchor">
+        <svg viewBox="0 0 200 170" aria-hidden="true">
+          <path className="brain-fill" d="M100 14c-27 0-46 15-52 33-14 3-24 16-24 31 0 9 4 17 10 23-3 6-4 12-4 18 0 22 19 40 44 40h52c25 0 44-18 44-40 0-6-1-12-4-18 6-6 10-14 10-23 0-15-10-28-24-31-6-18-25-33-52-33z"/>
+          <path className="brain-shade" d="M100 90c0 40 12 66 12 69h-24c-4-8-14-30-14-69 0-24 12-45 26-45s26 21 26 45c0 39-10 61-14 69"/>
+          <g className="brain-line">
+            <path d="M60 46c8-6 18-9 28-9M112 37c10 0 20 3 28 9M42 68c0-9 5-17 13-21M145 47c8 4 13 12 13 21"/>
+            <path d="M38 82c-5 6-8 13-8 20M162 82c5 6 8 13 8 20"/>
+            <path d="M72 45c4 10 4 22-2 30M128 45c-4 10-4 22 2 30"/>
+            <path d="M56 70c6 6 8 15 5 24M144 70c-6 6-8 15-5 24"/>
+            <path d="M100 30v130M84 60c4 20 4 45 16 70M116 60c-4 20-4 45-16 70"/>
+            <path d="M60 105c8 4 15 4 22 0M118 105c7 4 14 4 22 0"/>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
